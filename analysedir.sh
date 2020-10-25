aa85ce798cb55d3dbeae56b519479b6b37397871
n-peugnet <n.peugnet@free.fr>
2020-10-25 18:02:24 +0100

    fix: sort the files in the report
    by using sort and xargs
    
    Fixes #4

--- analysedir.sh
+++ analysedir.sh
@@ -4,4 +4,4 @@ export TMP_DIR=${TMP_DIR:-$(mktemp -dt cardbackup-XXXXXXXX)}
 
 rm -f $TMP_DIR/summary.tex
 
-find $1 -type f -exec ./analyse.sh "{}" \;
+find $1 -type f | sort -n | xargs -L1 -I% ./analyse.sh '%'
